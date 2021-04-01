6edacf3de7fb7e52c6c8f3a9445d095bad841797
Zach Green <zachjamesgreen@gmail.com>
2021-03-31 22:56:19 -0600

    Add another conditional to #valid_placement?

--- lib/board.rb
+++ lib/board.rb
@@ -10,12 +10,41 @@ class Board
     @cells.key?(coordinate)
   end
 
+  def valid_coordinates(coordinates)
+    coordinates.each do |coordinate|
+      if valid_coordinate?(coordinate) == false
+        return false
+      end
+      letters << coordinate[0]
+      numbers << coordinate[1]
+    end
+    if letters.uniq.length == 1 || numbers.uniq.length == 1
+      if letters.uniq.length != 1
+        letters.each_cons(2) do |letter|
+          if letter[0].ord - letter[1].ord == -1
+            return true
+          end
+        end
+      end
+
+      if numbers.uniq.length != 1
+        numbers.each_cons(2) do |number|
+          if number[0].ord - number[1].ord == -1
+            return true
+          end
+        end
+      end
+
+
+    end
+  end
+
   def valid_placement?(ship, coordinates)
     if ship.length == coordinates.length
       letters = []
       numbers = []
       coordinates.each do |coordinate|
-        if @cells[coordinate].empty?
+        unless @cells[coordinate].empty?
           return false
         end
         if valid_coordinate?(coordinate) == false
@@ -27,18 +56,21 @@ class Board
       if letters.uniq.length == 1 || numbers.uniq.length == 1
         if letters.uniq.length != 1
           letters.each_cons(2) do |letter|
-            if letter[0].ord - letter[1].ord == -1
-              return true
+            if letter[0].ord - letter[1].ord != -1
+
+              return false
             end
           end
+          return true
         end
 
         if numbers.uniq.length != 1
           numbers.each_cons(2) do |number|
-            if number[0].ord - number[1].ord == -1
-              return true
+            if number[0].ord - number[1].ord != -1
+              return false
             end
           end
+          return true
         end
 
 
